19a21a5599a4bc1007b7b6b8ab228b1ee6489ebd
Murat Cengiz <mcengiz240296@gmail.com>
2020-01-06 16:58:42 +0100

    Handle missing config.json file

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const rocketchat = require('./rocketchat');
-const config = require('./config');
+const fs = require('fs');
 
-rocketchat.runbot(config);
+const configFile = 'config.json';
+
+if (fs.existsSync(configFile)) {
+  const config = require('./config');
+  const rocketchat = require('./src/rocketchat');
+
+  rocketchat.runbot(config);
+} else {
+  console.error('"config.json" file not found. Please make sure to create it with necessary fields.');
+}
--- src/botpress.js
+++ src/botpress.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const config = require('./config');
+const config = require('../config');
 
 const instance = axios.create({
     baseURL: config.botpressHost + '/api/v1/bots',
