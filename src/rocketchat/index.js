4cb4b05c43fadca835e478f67252761d045e01ef
Murat Cengiz <mcengiz240296@gmail.com>
2020-01-07 16:20:46 +0100

    Different conversation for same user but different room

---
+++ src/rocketchat/index.js
@@ -0,0 +1,50 @@
+const { driver } = require('@rocket.chat/sdk');
+
+const utils = require('./utils');
+const botpress = require('../botpress');
+
+var myUserId;
+var botUsername;
+
+async function processMessages(error, message, messageOptions) {
+  if (!error) {
+    // Do not process own messages
+    if (message.u._id === myUserId) {
+      return;
+    }
+
+    let conversationId = message.u.username;
+    const roomName = await driver.getRoomName(message.rid);
+    if (roomName) {
+      conversationId += '-' + roomName;
+    }
+
+    const responses = await botpress.response(message.msg, conversationId, botUsername);
+    console.log('[responses]');
+    console.log(responses);
+
+    // Loop through each response message
+    for (const j in responses) {
+      const response = responses[j];
+      if (response.type === 'custom' && response.component === 'QuickReplies') { // Buttons message
+        await utils.sendAttachmentMessage(response, message.rid);
+      } else if (response.type === 'text') { // Text message
+        await utils.sendTextMessage(response, message.rid);
+      }
+    }
+  }
+}
+
+async function runbot(config) {
+  const conn = await driver.connect({ host: config.rocketchatHost, useSsl: config.ssl });
+  myUserId = await driver.login({ username: config.botUsername, password: config.botPassword });
+  botUsername = config.botUsername;
+
+  // set up subscriptions - rooms we are interested in listening to
+  const subscribed = await driver.subscribeToMessages();
+
+  // connect the processMessages callback
+  await driver.reactToMessages(processMessages);
+}
+
+exports.runbot = runbot;
---
+++ src/rocketchat/utils.js
@@ -0,0 +1,38 @@
+const { driver } = require('@rocket.chat/sdk');
+const he = require('he');
+
+async function sendTextMessage(response, roomId) {
+  const msg = await driver.prepareMessage(he.decode(response.text), roomId);
+  return await driver.sendMessage(msg);
+}
+
+async function sendAttachmentMessage(response, roomId) {
+  const msg = await driver.prepareMessage('', roomId);
+  const attachment = createButtons(response.wrapped.text, response.quick_replies);
+  msg.attachments = [attachment];
+
+  return await driver.sendMessage(msg);
+}
+
+function createButtons(title, replies) {
+  const attachment = {};
+  attachment.button_alignment = 'horizontal';
+  attachment.title = he.decode(title);
+
+  const actions = [];
+  for (const k in replies) {
+    const quickReply = replies[k];
+    actions.push({
+      type: 'button',
+      text: he.decode(quickReply.title),
+      msg: he.decode(quickReply.payload),
+      msg_in_chat_window: true,
+    });
+  }
+  attachment.actions = actions;
+
+  return attachment;
+}
+
+exports.sendTextMessage = sendTextMessage;
+exports.sendAttachmentMessage = sendAttachmentMessage;
